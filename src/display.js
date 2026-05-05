fda075766488f30f2c3ac0e965a53720dc5ddd61
M-Zain-924823 <mz924823@gmail.com>
2026-05-05 22:05:13 +0500

    Wiring! (Connecting Display with Classes)

--- src/display.js
+++ src/display.js
@@ -1,33 +1,77 @@
+function makeBoardEmpty(id) {
+    const gameboard = document.querySelector(`#${id}`);
+    gameboard.innerHTML = "";
+}
+
 function createGrid() {
     const gameboard = document.querySelectorAll(".game-board");
     gameboard.forEach(board => {
-        for (let i = 0; i < 100; i++) {
+        makeBoardEmpty(board.id);
+        for (let i = 1; i <= 100; i++) {
             const div = document.createElement("div");
+            div.id = i;
             board.appendChild(div);
         }
     });
 }
 
-function createCell(type) {
+function createCell(type, player = false) {
     const div = document.createElement("div");
     if (type === -1) 
-        return div.classList.add("missed");
+        div.classList.add("missed");
     else if (type === 0) 
-        return div.classList.add("hit");
-    else return div;
+        div.classList.add("hit");
+    else if (type !== null && player)
+        div.classList.add("ship");
+    return div;
 }
 
-function displayGameBoard(gameBoard) {
-    const displayBoards = document.querySelectorAll('.game-board');
-    displayBoards.forEach(board => {
-        for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
-                const div = createCell(gameBoard[i][j]);
-                board.appendChild(div);
-            }
+function displayGameBoard(type, gameBoard) {
+    makeBoardEmpty(type);
+    const displayBoard = document.querySelector(`#${type}`);
+    for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+            const div = createCell(gameBoard[i][j], type === "player-board");
+            displayBoard.appendChild(div);
         }
-    });
+    }
 }
 
-// Don't use displayGameBoard yet! Fix it first!
-export {createGrid, displayGameBoard};
+function createShips(board) {
+    // const sorted = [];
+    // for (let x = 1; x <= 10; x++) {
+    //     for (let y = 1; y <= 10; y++) {
+    //         sorted.push([x, y]);
+    //     }
+    // }
+
+    // const ships = [];
+    // board.forEach(cell => {
+    //     if (cell.class === "ship") {
+    //         ships.push({
+    //             coords: [sorted[cell.id]],
+    //             length: cell.size(),
+    //             axis: (cell.size() % 2) ? y : x,
+    //         }); // horizontal axis for odd size ships, vertical axis for all even size ships
+    //     };
+    // });
+
+    const ships = [
+        {coords: [1, 4], length: 5, axis: 'x'},
+        {coords: [2, 2], length: 4, axis: 'y'},
+        {coords: [6, 7], length: 3, axis: 'x'},
+        {coords: [9, 10], length: 2, axis: 'y'},
+        {coords: [4, 6], length: 2, axis: 'x'},
+        {coords: [10, 1], length: 1, axis: 'x'},
+        {coords: [8, 3], length: 1, axis: 'x'},
+    ];
+    return ships;
+}
+
+function getShips(id) {
+    const board = document.querySelector(`#${id}`);
+    const ships = createShips(board);
+    return ships;
+}
+
+export {createGrid, getShips, displayGameBoard};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import "./styles.css";
-import { createGrid } from "./display";
+import { createGrid, getShips, displayGameBoard } from "./display";
+import Player from "./Player";
 
 createGrid();
+
+const startBtn = document.querySelector("#start-btn");
+startBtn.addEventListener("click", () => {
+    const playerShips = getShips("player-board");
+    const computerShips = getShips("computer-board");
+    startGame(playerShips, computerShips);
+});
+
+function startGame(playerShips, computerShips) {
+    const humanPlayer = new Player(playerShips);
+    const computerPlayer = new Player(computerShips);
+    displayGameBoard("player-board", humanPlayer.getGameBoard());
+    displayGameBoard("computer-board", computerPlayer.getGameBoard());
+}
