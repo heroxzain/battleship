e6a54ba0bce6c90fa9d09fdcf0d6478879a1afdc
M-Zain-924823 <mz924823@gmail.com>
2026-05-08 09:50:46 +0500

    add random ships placement

--- src/GenerateShips.js
+++ src/GenerateShips.js
@@ -1,12 +1,49 @@
 export default function GenerateShips() {
-    const ships = [
-        {coords: [1, 4], length: 5, axis: 'x'},
-        {coords: [2, 2], length: 4, axis: 'y'},
-        {coords: [6, 7], length: 3, axis: 'x'},
-        {coords: [9, 10], length: 2, axis: 'y'},
-        {coords: [4, 6], length: 2, axis: 'x'},
-        {coords: [10, 1], length: 1, axis: 'x'},
-        {coords: [8, 3], length: 1, axis: 'x'},
-    ];
-    return ships;
+    const visited = [];
+    const finalShips = [];
+    const roster = [5, 4, 3, 2, 2, 1, 1];
+
+    roster.forEach(length => {
+        let isPlaced = false;
+
+        while (!isPlaced) {
+            const axis = Math.random() > 0.5 ? "x" : "y";
+            const startX = Math.floor(Math.random() * 10) + 1;
+            const startY = Math.floor(Math.random() * 10) + 1;
+
+            let proposedCoords = [];
+            let isValid = true;
+
+            for (let i = 0; i < length; i++) {
+                let currentX = axis === "y" ? startX + i : startX;
+                let currentY = axis === "x" ? startY + i : startY;
+
+                if (currentX > 10 || currentY > 10) {
+                    isValid = false;
+                    break; 
+                }
+
+                if (visited.includes(`${currentX},${currentY}`)) {
+                    isValid = false;
+                    break;
+                }
+
+                proposedCoords.push([currentX, currentY]);
+            }
+
+            if (isValid) {
+                proposedCoords.forEach(coord => visited.push(`${coord[0]},${coord[1]}`));
+                
+                finalShips.push({
+                    coords: [startX, startY],
+                    length: length,
+                    axis: axis
+                });
+
+                isPlaced = true; 
+            }
+        }
+    });
+
+    return finalShips;
 }
