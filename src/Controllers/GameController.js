ab413f9599942b34af66a589cf69636a70e504d8
M-Zain-924823 <mz924823@gmail.com>
2026-05-15 14:13:02 +0500

    feat: bonus turn on hit

--- src/Controllers/GameController.js
+++ src/Controllers/GameController.js
@@ -23,17 +23,22 @@ export default function GameController(playerShips, computerShips) {
     }
 
     const playRound = (e) => {
-        if (!isPlayerTurn) return; // Prevent clicking during computer's turn
+        if (!isPlayerTurn) return;
         if (!e.target.dataset.row) return; 
         if (e.target.classList.contains("hit") || e.target.classList.contains("missed")) return;
         const coords = createCoords(e);
         computerPlayer.attackTheShip(coords);
         syncGUI("computer-board", computerPlayer.getGameBoard());
         displayRemainingShips("computer-ships", computerPlayer.getShips());
-        isPlayerTurn = false;
         if (computerPlayer.checkWinner()) 
             return displayWinner("Player", computerPlayer.getGameBoard());
-        displayMessage("Computer is thinking...");
+        const isHit = computerPlayer.getGameBoard()[coords[0] - 1][coords[1] - 1] === 0;
+        if (isHit) {
+            displayMessage("You Hit a ship! Fire again...");
+            return;
+        }
+        isPlayerTurn = false;
+        displayMessage("You Missed! Computer is thinking...");
         setTimeout(() => { computerAttack(); }, 1000);
     };
 
@@ -45,8 +50,14 @@ export default function GameController(playerShips, computerShips) {
         displayRemainingShips("player-ships", humanPlayer.getShips());
         if (humanPlayer.checkWinner())
             return displayWinner("Computer", computerPlayer.getGameBoard());
+        const isHit = humanPlayer.getGameBoard()[coords[0] - 1][coords[1] - 1] === 0;
+        if (isHit) {
+            displayMessage("Enemy Hit your ship! Computer is thinking again...");
+            setTimeout(() => { computerAttack(); }, 1000);
+            return; 
+        }
         isPlayerTurn = true;
-        displayMessage("Your Turn...");
+        displayMessage("Enemy Missed! Your Turn...");
     }
 
     const syncGUI = (type, board) => {
